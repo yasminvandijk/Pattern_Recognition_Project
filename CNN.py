3a35feac01795851c24636a737002be807210a30
anneloesmeijer <a.l.meijer2@students.uu.nl>
2021-01-22 16:46:30 +0100

    training model of sign images, error in dimensions setting

--- CNN.py
+++ CNN.py
@@ -6,18 +6,18 @@ import SqueezeNet
 import DataLoad
 
 (X,Y) = DataLoad.load_data()
+X = X / 255.0
 
-# import Cifar dataset
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+#(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+#train_images, test_images = train_images / 255.0, test_images / 255.0
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+#class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+#               'dog', 'frog', 'horse', 'ship', 'truck']
 
 # Create the model. Give the dimensions of the input data as parameter
-model = SqueezeNet.SqueezeNet(10, inputs=(32, 32, 3))
+model = SqueezeNet.SqueezeNet(43, inputs=(48, 48, 3))
 
 # Compile and train the model
 model.compile(optimizer='adam',
@@ -25,22 +25,22 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 history = model.fit(
-                train_images,
-                train_labels,
-                epochs=4,
-                steps_per_epoch=400,
-                validation_data=(test_images, test_labels))
+                X,
+                Y,
+                epochs=2,
+                steps_per_epoch=200)
+                #validation_data=(test_images, test_labels))
 
 
 # Evaluate the model
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
-plt.show()
+# plt.plot(history.history['accuracy'], label='accuracy')
+# #plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.5, 1])
+# plt.legend(loc='lower right')
+# plt.show()
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+#test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print(test_acc)
+#print(test_acc)
